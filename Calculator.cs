6d33f48f9f56096997afb58dc77cd0a646117edd
emolesky <emilynnmolesky@gmail.com>
2025-02-16 19:00:44 -0600

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+﻿//Emilynn Molesky
+//2/16/25
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLetter
+{
+    public class Calculator
+    {
+        public string Move(string str)
+        {
+            string result = "";
+
+            foreach(char c in str)
+            {
+                result += (char)(c + 1);
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NextLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator aCalculator = new Calculator();
+            Console.WriteLine(aCalculator.Move("hello"));
+        }
+    }
+}
